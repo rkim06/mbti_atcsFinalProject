68394f2ad2e4281a42dc6edb282821df748d295f
rkim06 <rena.kim@menloschool.org>
2024-05-02 13:41:22 -0700

    map line and change

--- mbtiData.py
+++ mbtiData.py
@@ -1,6 +1,6 @@
 class MBTI:
     
-    def __init__(self, type, posts=None):
+    def __init__(self, type, posts=[]):
         self.type = type
         self.posts = posts
    
@@ -10,23 +10,21 @@ class MBTI:
     def get_type(self):
         return self.type
 
-    def add_posts(toAdd_posts):
-        posts += toAdd_posts
-        
+    def add_posts(self, toAdd_posts):
         # Organize the posts so that they can be distinguished/easily accesible as individual pieces of text
-        posts = posts.split("|||")
+        toAdd_posts = toAdd_posts.split("|||")
+
+        for i in toAdd_posts:
+            self.posts.append(i)
 
 
-    def clean_posts():
-        # Organize the posts so that they can be distinguished/easily accesible as individual pieces of text
-        posts = posts.split("|||")
-
-        for pst in posts:
+    def clean_posts(self):
+        for pst in self.posts:
             # Get rid of all the posts with links because model can't get access to the content of a video
             # Some of the data is just numbers which, without context or further info, doesn't provide any info
             # And empty data is unnecessary, will confuse or confound analysis
             if pst.contains('https:') or pst.isdigit() or pst == '':
-                posts.remove(pst)
+                self.posts.remove(pst)
                 
     # If the types are the same, then combine into one list of posts
     def organize_posts_byType(self, otherType):
